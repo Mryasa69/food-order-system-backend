0d54036be2580277d6bdf4fff08a0ca401ed2f8c
Mryasa69 <yasaswickramarathna32@gmail.com>
2026-04-02 12:54:42 +0530

    update order routes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ import orderModel from "../models/orderModel.js";
 import userModel from '../models/userModel.js'
 import Stripe from "stripe"
 
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order for frontend
@@ -100,5 +101,16 @@ const listOrders = async (req, res) => {
     }
 }
 
-export { placeOrder, verifyOrder, userOrders, listOrders }
+// api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" });
+    }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus }
 
